b52dbf157d8d023ac427223e97eb7f1816964854
HectorGarcia01 <hg2779948@gmail.com>
2023-05-24 02:14:29 -0600

    Agregando la configuración de la bd y la importación de los modelos en el archivo main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 #Creamos una instancia de Flask y definimos la ruta static
 app = Flask(__name__, static_url_path='/static')
+
+#Cargamos las configuraciones de la bd
+app.config.from_pyfile('./config/config.py')
+
+#Configuración de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{app.config['DB_USER']}:{app.config['DB_PASSWORD']}@{app.config['DB_HOST']}/{app.config['DB_NAME']}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Creamos una instancia de SQLAlchemy
+db = SQLAlchemy(app)
+
+#Importamos los modelos
+from models.usuario import Usuario
+from models.prediccion import Prediccion
 
 #Configuramos la ruta raíz para redirigir al login
 @app.route('/')
