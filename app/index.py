c3d40fa928525fcc798e5d836677b81d384e918c
HectorGarcia01 <hg2779948@gmail.com>
2023-06-01 17:47:00 -0600

    Cargando la instancia db para crear todas las tablas en la base de datos cuando arranque el programa

--- app/index.py
+++ app/index.py
@@ -1,4 +1,9 @@
-from main import app #Cargamos el archivo main que posee la configuración de la aplicación
+from main import app                #Cargamos el archivo main que posee la configuración de la aplicación
+from db.conexionBD import db        #Cargamos la instancia db
+
+#Para que cree todas las tablas que hemos definido en los modelos
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     #Inicializamos el servidor web
