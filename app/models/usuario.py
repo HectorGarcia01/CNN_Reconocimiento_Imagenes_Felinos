d89819d120cdffe15c02f56eab39decb490c0d57
HectorGarcia01 <hg2779948@gmail.com>
2023-06-01 17:54:07 -0600

    Creación del constructor de la clase Usuario

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,3 +1,4 @@
+from passlib.hash import pbkdf2_sha256
 from db.conexionBD import db #Cargamos la instacia db
 
 class Usuario(db.Model):
@@ -8,4 +9,10 @@ class Usuario(db.Model):
     correo = db.Column(db.String(30), unique=True)     #Definimos el atributo para el correo del usuario y la establecemos como unica 
     password = db.Column(db.String(30))                #Definimos el atributo para la contraseña del usuario
 
-    
+    #Definimos el constructor para pasar los datos a la bd
+    def __init__(self, nombre, apellido, direccion, correo, password):
+        self.nombre = nombre                            #Asignamos el valor proporcionado al atributo nombre
+        self.apellido = apellido                        #Asignamos el valor proporcionado al atributo nombre
+        self.direccion = direccion                      #Asignamos el valor proporcionado al atributo nombre
+        self.correo = correo                            #Asignamos el valor proporcionado al atributo nombre
+        self.password = pbkdf2_sha256.hash(password)    #Asignamos el valor proporcionado al atributo password pero encriptada con sha256
