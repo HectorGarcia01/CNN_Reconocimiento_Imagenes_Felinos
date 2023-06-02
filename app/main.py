a01b591526839968a1292548d1dcf3be9e95ac90
HectorGarcia01 <hg2779948@gmail.com>
2023-06-01 19:31:31 -0600

    Reestructuración de la configuración del main.py y creación de una función para agregar las rutas a la aplicación

--- app/main.py
+++ app/main.py
@@ -1,21 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config.config import *
-from routes.usuario import rutas_usuario            #Cargamos las rutas del usuario
-from routes.prediccion import rutas_prediccion      #Cargamos las rutas de las predicciones
 
 #Creamos una instancia de Flask y definimos la ruta static
 app = Flask(__name__)
 
 #Configuramos la bd
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{USUARIO_BD}:{PASSWORD_BD}@{NOMBRE_HOST}/{NOMBRE_BD}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://hector_garcia:47980664DD@localhost/prediccion_felinos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Pasamos la instancia de app a SQLAlchemy
-SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-#Agregamos las rutas del usuario a la aplicación
-app.register_blueprint(rutas_usuario)
+#Definimos una función para agregar las rutas a la instancia app
+def crear_app():
+    from routes.usuario import rutas_usuario            #Cargamos las rutas del usuario
+    from routes.prediccion import rutas_prediccion      #Cargamos las rutas de las predicciones
 
-#Agregamos las rutas de las predicciones
-app.register_blueprint(rutas_prediccion)
+    #Agregamos las rutas del usuario a la aplicación
+    app.register_blueprint(rutas_usuario)
+
+    #Agregamos las rutas de las predicciones
+    app.register_blueprint(rutas_prediccion)
+
+    #Retornamos la instancia app
+    return app
