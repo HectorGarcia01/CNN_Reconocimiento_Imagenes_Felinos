9d077d096b9d813e4408b72e3567cf7f48785471
HectorGarcia01 <hg2779948@gmail.com>
2023-06-01 21:31:46 -0600

    Agregando excepción de errores en la función validar_credenciales

--- app/controllers/usuario_controller.py
+++ app/controllers/usuario_controller.py
@@ -24,21 +24,26 @@ def guardar_usuario():
         #Redireccionamos a la ruta para el registro de usuario
         return redirect('/registro/usuario')
     except Exception as e:
+        #Redireccionamos a la ruta para el registro de usuario
         return redirect('/registro/usuario')
 
 #Definimos la función para validar las credenciales del usuario
 def validar_credenciales():
-    #Obtenemos los datos del formulario
-    correo = request.form['correo']
-    password = request.form['password']
+    try:
+        #Obtenemos los datos del formulario
+        correo = request.form['correo']
+        password = request.form['password']
 
-    #Buscamos el usuario en la bd por su correo
-    usuario = Usuario.query.filter_by(correo=correo).first()
+        #Buscamos el usuario en la bd por su correo
+        usuario = Usuario.query.filter_by(correo=correo).first()
 
-    #Validamos si las contraseñas coinciden
-    if usuario and usuario.verificar_contraseña(password):
-        #Redireccionamos a la ruta para la predicción
-        return redirect('/prediccion/felinos')
-    else:
+        #Validamos si las contraseñas coinciden
+        if usuario and usuario.verificar_contraseña(password):
+            #Redireccionamos a la ruta para la predicción
+            return redirect('/prediccion/felinos')
+        else:
+            #Redireccionamos al mismo login
+            return redirect('/login')
+    except Exception as e:
         #Redireccionamos al mismo login
         return redirect('/login')
