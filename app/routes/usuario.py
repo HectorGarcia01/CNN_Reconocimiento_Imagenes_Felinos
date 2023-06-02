4a92dc1dbe9ef1203e06cff4d440e599684aadd1
HectorGarcia01 <hg2779948@gmail.com>
2023-06-01 19:58:29 -0600

    Separando la lógica del registro a la bd

--- app/routes/usuario.py
+++ app/routes/usuario.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect, request
-from models.usuario import Usuario      #Cargamos la clase Usuario
-from main import db                     #Cargamos la instancia db
+from controllers.usuario_controller import guardar_usuario      #Cargamos la función guardar_usuario del controlador
 
 #Creamos la sección para las rutas de usuarios
 rutas_usuario = Blueprint('usuario', __name__)
@@ -25,21 +24,4 @@ def registro():
 #Configuramos la ruta para guardar los datos del usuario
 @rutas_usuario.route('/registro/nuevo_usuario', methods=['POST'])
 def nuevo_usuario():
-    #Obtenemos los datos del formulario
-    nombre = request.form['nombre']
-    apellido = request.form['apellido']
-    direccion = request.form['direccion']
-    correo = request.form['correo']
-    password = request.form['password']
-
-    #Instanciamos la clase Usuario para crear uno nuevo objeto
-    nuevo_usuario = Usuario(nombre, apellido, direccion, correo, password)
-
-    #Guardamos el nuevo usuario
-    db.session.add(nuevo_usuario)
-
-    #Finalizamos la conexión para que guarde el nuevo usuario a la bd
-    db.session.commit()
-
-    #Redireccionamos a la ruta para el registro de usuario
-    return redirect('/registro/usuario')
+    return guardar_usuario()
