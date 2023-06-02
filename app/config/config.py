422c8cf20bd9877dada81787b5987e409d5137e8
HectorGarcia01 <hg2779948@gmail.com>
2023-06-02 02:57:08 -0600

    Definiendo nuestra configuración para la conexión de la bd utilizando variables de entorno

---
+++ app/config/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#Obtenemos nuestras constantes de configuración del .env
+host = os.environ['NOMBRE_HOST']
+usuario = os.environ['USUARIO_BD']
+password = os.environ['PASSWORD_BD']
+nombre_bd = os.environ['NOMBRE_BD']
+
+#Definimos la URL de conexión a la base de datos
+URI_CONEXION_BD = f'mysql://{usuario}:{password}@{host}/{nombre_bd}'
