0503c5ccf834bf39dc8b28ac64f5c3e3432db527
HectorGarcia01 <hg2779948@gmail.com>
2023-06-01 23:23:58 -0600

    Eliminación de la función para validar las credenciales

--- app/controllers/usuario_controller.py
+++ app/controllers/usuario_controller.py
@@ -26,24 +26,3 @@ def guardar_usuario():
     except Exception as e:
         #Redireccionamos a la ruta para el registro de usuario
         return redirect('/registro/usuario')
-
-#Definimos la función para validar las credenciales del usuario
-def validar_credenciales():
-    try:
-        #Obtenemos los datos del formulario
-        correo = request.form['correo']
-        password = request.form['password']
-
-        #Buscamos el usuario en la bd por su correo
-        usuario = Usuario.query.filter_by(correo=correo).first()
-
-        #Validamos si las contraseñas coinciden
-        if usuario and usuario.verificar_contraseña(password):
-            #Redireccionamos a la ruta para la predicción
-            return redirect('/prediccion/felinos')
-        else:
-            #Redireccionamos al mismo login
-            return redirect('/login')
-    except Exception as e:
-        #Redireccionamos al mismo login
-        return redirect('/login')
