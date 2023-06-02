6cd202f43f649064e0b8fd4cfe6023674febad88
HectorGarcia01 <hg2779948@gmail.com>
2023-06-02 05:32:12 -0600

    Agregando la clave secreta de flask para el acceso al metodo de sesiones

--- app/config/config.py
+++ app/config/config.py
@@ -8,6 +8,7 @@ host = os.environ['NOMBRE_HOST']
 usuario = os.environ['USUARIO_BD']
 password = os.environ['PASSWORD_BD']
 nombre_bd = os.environ['NOMBRE_BD']
+clave_secreta_flask = os.environ['CLAVE_FLASK']
 
 #Definimos la URL de conexión a la base de datos
 URI_CONEXION_BD = f'mysql://{usuario}:{password}@{host}/{nombre_bd}'
