fe19f43b7c25fd32a5125f641248bd559caaa92e
HectorGarcia01 <hg2779948@gmail.com>
2023-06-02 21:33:42 -0600

    Actualización de la función de la ruta para la prediccion

--- app/routes/prediccion.py
+++ app/routes/prediccion.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint 
 from controllers.autenticacion_controller import verificar_sesion_pred           #Cargamos la función verificar_sesion_pred del controlador
+from controllers.prediccion_controller import guardar_prediccion                 #Cargamos la función guardar_prediccion del controlador
 
 #Creamos la sección para las rutas de predicción
 rutas_prediccion = Blueprint('prediccion', __name__)
@@ -9,3 +10,9 @@ rutas_prediccion = Blueprint('prediccion', __name__)
 def prediccion():
     #Retornamos la función verificar_sesion_pred
     return verificar_sesion_pred()
+
+#Configuramos la ruta para guardar los datos del usuario
+@rutas_prediccion.route('/prediccion/nueva_prediccion', methods=['POST'])
+def nueva_prediccion():
+    #Retornamos la función para guardar la nueva prediccion a la bd
+    return guardar_prediccion()
